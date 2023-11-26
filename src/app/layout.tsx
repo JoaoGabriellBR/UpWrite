77748c8170d96ca9c8ea42461193a13c79b3f569
João Gabriel <joaoname9@gmail.com>
2023-11-26 15:33:54 -0300

    feat: suppressHydrationWarning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={cn('min-h-[100dvh] bg-background font-sans antialiased', fontSans.variable, fontHeading.variable)}
       >
