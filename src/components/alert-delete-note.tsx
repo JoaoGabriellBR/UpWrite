123f637f6e6d7d98fe264ce04cffd9d013f5e269
João Gabriel <joaoname9@gmail.com>
2023-11-30 14:54:26 -0300

    feat: adding close icon CreateNote Route

--- src/components/alert-delete-note.tsx
+++ src/components/alert-delete-note.tsx
@@ -18,10 +18,13 @@ import {
 import { Icons } from "./icons";
 import { Button } from "./ui/button";
 import { useState, useEffect } from 'react'
+import { usePathname } from 'next/navigation'
+import Link from "next/link";
 
 export default function AlertDeleteNote({ handleClickDelete }: any) {
 
-    const [isClient, setIsClient] = useState(false)
+    const [isClient, setIsClient] = useState(false);
+    const pathname = usePathname();
 
     useEffect(() => {
         setIsClient(true)
@@ -30,35 +33,44 @@ export default function AlertDeleteNote({ handleClickDelete }: any) {
     return (
         <>
             {isClient ? (
-                <AlertDialog>
-                    <AlertDialogTrigger>
-                        <DropdownMenu>
-                            <DropdownMenuTrigger>
-                                <Button variant="outline">
-                                    <Icons.moreHorizontal className="h-5 w-5" />
-                                </Button>
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent align="end" className="w-48">
-                                <DropdownMenuItem className="cursor-pointer">
-                                    <Icons.trash className="mr-2 h-4 w-4" />
-                                    <p>Excluir nota</p>
-                                </DropdownMenuItem>
-                            </DropdownMenuContent>
-                        </DropdownMenu>
-                    </AlertDialogTrigger>
-                    <AlertDialogContent>
-                        <AlertDialogHeader>
-                            <AlertDialogTitle>Você tem certeza?</AlertDialogTitle>
-                            <AlertDialogDescription>
-                                Essa ação não pode ser desfeita. Isso excluirá permanentemente sua nota.
-                            </AlertDialogDescription>
-                        </AlertDialogHeader>
-                        <AlertDialogFooter>
-                            <AlertDialogCancel>Cancelar</AlertDialogCancel>
-                            <AlertDialogAction onClick={handleClickDelete}>Excluir</AlertDialogAction>
-                        </AlertDialogFooter>
-                    </AlertDialogContent>
-                </AlertDialog>
+                pathname === "/createnote" ? (
+                    <Link href="/notes">
+                        <Button variant="outline">
+                            <Icons.close className="h-5 w-5" />
+                        </Button>
+                    </Link>
+                ) : (
+                    <AlertDialog>
+                        <AlertDialogTrigger>
+                            <DropdownMenu>
+                                <DropdownMenuTrigger>
+                                    <Button variant="outline">
+                                        <Icons.moreHorizontal className="h-5 w-5" />
+                                    </Button>
+                                </DropdownMenuTrigger>
+
+                                <DropdownMenuContent align="end" className="w-48">
+                                    <DropdownMenuItem className="cursor-pointer">
+                                        <Icons.trash className="mr-2 h-4 w-4" />
+                                        <p>Excluir nota</p>
+                                    </DropdownMenuItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent>
+                            <AlertDialogHeader>
+                                <AlertDialogTitle>Você tem certeza?</AlertDialogTitle>
+                                <AlertDialogDescription>
+                                    Essa ação não pode ser desfeita. Isso excluirá permanentemente sua nota.
+                                </AlertDialogDescription>
+                            </AlertDialogHeader>
+                            <AlertDialogFooter>
+                                <AlertDialogCancel>Cancelar</AlertDialogCancel>
+                                <AlertDialogAction onClick={handleClickDelete}>Excluir</AlertDialogAction>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialog>
+                )
             ) : null}
         </>
     )
