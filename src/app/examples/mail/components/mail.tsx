d2b47985d63b63165526ebbeb6fb72bf8cf486a6
João Gabriel <joaoname9@gmail.com>
2024-01-15 19:10:20 -0300

    feat: adding examples/mail

---
+++ src/app/examples/mail/components/mail.tsx
@@ -0,0 +1,169 @@
+"use client"
+import * as React from "react"
+import {
+  AlertCircle,
+  Archive,
+  ArchiveX,
+  File,
+  Inbox,
+  LogOut,
+  MessagesSquare,
+  PenBox,
+  Plus,
+  Search,
+  Send,
+  Settings,
+  ShoppingCart,
+  Trash2,
+  Users2,
+} from "lucide-react"
+
+import { MailDisplay } from "@/app/examples/mail/components/mail-display"
+import { MailList } from "@/app/examples/mail/components/mail-list"
+import { Nav } from "@/app/examples/mail/components/nav"
+import { Mail as MailData } from "@/app/examples/mail/data"
+import { useMail } from "@/app/examples/mail/use-mail"
+import { cn } from "@/lib/utils"
+import { Separator } from "@/components/ui/separator"
+import { Input } from "@/components/ui/input"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+import { TooltipProvider } from "@/components/ui/tooltip"
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable"
+import DropdownAvatar from "@/components/dropdown-avatar"
+
+interface MailProps {
+  accounts: {
+    label: string
+    email: string
+    icon: React.ReactNode
+  }[]
+  mails: MailData[]
+  defaultLayout?: number[] | undefined
+  defaultCollapsed?: boolean
+  navCollapsedSize: number
+}
+
+export function Mail({
+  accounts,
+  mails,
+  defaultLayout = [265, 440, 655],
+  defaultCollapsed = false,
+  navCollapsedSize,
+}: MailProps) {
+  const [isCollapsed, setIsCollapsed] = React.useState(defaultCollapsed)
+  const [mail] = useMail()
+
+  return (
+    <TooltipProvider delayDuration={0}>
+      <ResizablePanelGroup
+        direction="horizontal"
+        onLayout={(sizes: number[]) => {
+          document.cookie = `react-resizable-panels:layout=${JSON.stringify(
+            sizes
+          )}`
+        }}
+        className="h-full max-h-[800px] items-stretch"
+      >
+        <ResizablePanel
+          defaultSize={defaultLayout[0]}
+          collapsedSize={navCollapsedSize}
+          collapsible={true}
+          minSize={15}
+          maxSize={20}
+          onCollapse={() => setIsCollapsed(true)}
+          onExpand={() => setIsCollapsed(false)}
+          className={cn(isCollapsed && "min-w-[50px] transition-all duration-300 ease-in-out")}
+        >
+          <div className={cn("flex h-[52px] items-center justify-center", isCollapsed ? 'h-[52px]': 'px-2')}>
+             <DropdownAvatar/>
+          </div>
+          <Separator />
+          <Nav
+            isCollapsed={isCollapsed}
+            links={[
+              {
+                title: "Nova nota",
+                label: "",
+                icon: Plus,
+                variant: "default",
+              },
+              {
+                title: "Notas",
+                label: "9",
+                icon: File,
+                variant: "ghost",
+              },
+              {
+                title: "Configurações",
+                label: "",
+                icon: Settings,
+                variant: "ghost",
+              },
+              {
+                title: "Lixeira",
+                label: "",
+                icon: Trash2,
+                variant: "ghost",
+              },
+              {
+                title: "Arquivadas",
+                label: "",
+                icon: Archive,
+                variant: "ghost",
+              },
+              {
+                title: "Sair",
+                label: "",
+                icon: LogOut,
+                variant: "ghost",
+              },
+            ]}
+          />
+        </ResizablePanel>
+
+        <ResizableHandle withHandle />
+
+        <ResizablePanel defaultSize={defaultLayout[1]} minSize={20}>
+          <Tabs defaultValue="all">
+            <div className="flex items-center px-4 py-2">
+              <h1 className="text-xl font-bold">Notas</h1>
+              <TabsList className="ml-auto">
+                <TabsTrigger value="all" className="text-zinc-600 dark:text-zinc-200">All mail</TabsTrigger>
+                <TabsTrigger value="unread" className="text-zinc-600 dark:text-zinc-200">Unread</TabsTrigger>
+              </TabsList>
+            </div>
+            <Separator />
+            <div className="bg-background/95 p-4 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+              <form>
+                <div className="relative">
+                  <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
+                  <Input placeholder="Search" className="pl-8" />
+                </div>
+              </form>
+            </div>
+            <TabsContent value="all" className="m-0">
+              <MailList items={mails} />
+            </TabsContent>
+            <TabsContent value="unread" className="m-0">
+              <MailList items={mails.filter((item) => !item.read)} />
+            </TabsContent>
+          </Tabs>
+        </ResizablePanel>
+        
+        <ResizableHandle withHandle />
+
+        <ResizablePanel defaultSize={defaultLayout[2]}>
+          <MailDisplay
+            mail={mails.find((item) => item.id === mail.selected) || null}
+          />
+        </ResizablePanel>
+
+      </ResizablePanelGroup>
+    </TooltipProvider>
+  )
+}
