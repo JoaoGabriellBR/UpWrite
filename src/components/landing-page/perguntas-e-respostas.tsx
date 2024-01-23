ff67c135a390491acefadf18bf28a899b3af2184
João Gabriel <joaoname9@gmail.com>
2024-01-23 10:37:03 -0300

    refactor: Accordion Landing Page

--- src/components/landing-page/perguntas-e-respostas.tsx
+++ src/components/landing-page/perguntas-e-respostas.tsx
@@ -5,10 +5,38 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { CheckCircle } from "lucide-react";
+
+interface AccordionProps {
+  value: string;
+  question: string;
+  answer: string;
+}
 
 const PerguntasRespostas = () => {
+  const accordions = [
+    {
+      value: "item-1",
+      question: "Quantas notas posso criar?",
+      answer: "Você pode criar quantas notas desejar. (:",
+    },
+    {
+      value: "item-2",
+      question: "Posso editar minha nota após a publicação?",
+      answer: "Sim, é possível editar uma nota enquanto você trabalha nela.",
+    },
+    {
+      value: "item-3",
+      question: "Consigo recuperar notas excluídas?",
+      answer:
+        "Por enquanto não. Após excluída, a sua nota será automaticamente retirada de nosso banco de dados.",
+    },
+  ];
   return (
-    <section id="faq" className="py-8 md:py-20 w-full flex flex-col justify-center items-center">
+    <section
+      id="faq"
+      className="py-8 md:py-20 w-full flex flex-col justify-center items-center"
+    >
       <div className="container flex max-w-6xl flex-col items-center gap-4 text-center">
         <h1 className="font-heading text-4xl sm:text-4xl md:text-5xl lg:text-6xl">
           Perguntas e Respostas
@@ -19,31 +47,19 @@ const PerguntasRespostas = () => {
         </p>
 
         <Accordion type="single" collapsible className="w-full text-start">
-          <AccordionItem value="item-1">
-            <AccordionTrigger>Quantas notas posso criar?</AccordionTrigger>
-            <AccordionContent>
-              Você pode criar quantas notas desejar. (:
-            </AccordionContent>
-          </AccordionItem>
-
-          <AccordionItem value="item-2">
-            <AccordionTrigger>
-              Posso editar minha nota após a publicação?
-            </AccordionTrigger>
-            <AccordionContent>
-              Sim, é possível editar uma nota enquanto você trabalha nela.
-            </AccordionContent>
-          </AccordionItem>
-
-          <AccordionItem value="item-3">
-            <AccordionTrigger>
-              Consigo recuperar notas excluídas?
-            </AccordionTrigger>
-            <AccordionContent>
-              Não. Após excluída, a sua nota será automaticamente retirada de
-              nosso banco de dados.
-            </AccordionContent>
-          </AccordionItem>
+          {accordions.map((accordion: AccordionProps) => (
+            <AccordionItem key={accordion.value} value={accordion.value}>
+              <AccordionTrigger>
+                <div className="flex flex-row justify-start items-center gap-2">
+                  <CheckCircle className="text-primary w-4 h-4" />
+                  <span className="text-sm sm:text-sm md:text-base lg:text-base">
+                    {accordion.question}
+                  </span>
+                </div>
+              </AccordionTrigger>
+              <AccordionContent>{accordion.answer}</AccordionContent>
+            </AccordionItem>
+          ))}
         </Accordion>
       </div>
     </section>
