eae9422be0e4b92d6f0ff5dc27460a1b0897ce1f
João Gabriel <joaoname9@gmail.com>
2024-01-30 10:44:26 -0300

    fix: resolvedTheme images

--- src/components/landing-page/organizaçao.tsx
+++ src/components/landing-page/organizaçao.tsx
@@ -1,20 +1,29 @@
-"use client"
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import organizacaoLightMode from "../../../public/organizacao-lightmode.png";
 import organizacaoDarkMode from "../../../public/organizacao-darkmode.png";
 import ComponentSchema from "./component-schema";
 import { useTheme } from "next-themes";
 
-const Organizaçao = () => {
+const Organizacao = () => {
   const { resolvedTheme } = useTheme();
-  const images = resolvedTheme === "dark" ? organizacaoDarkMode : organizacaoLightMode;
+  const [imageSrc, setImageSrc] = useState({});
+
+  useEffect(() => {
+    if (resolvedTheme === "dark") {
+      setImageSrc(organizacaoDarkMode);
+    } else {
+      setImageSrc(organizacaoLightMode);
+    }
+  }, [resolvedTheme]);
+
   return (
     <>
       <ComponentSchema
         id="caracteristicas"
-        title="Organização flexível"
-        description="O UpWrite oferece liberdade total na organização. Você pode criar um sistema de cadernos ou simplesmente dispensar a organização. Todas as suas notas estão a apenas uma pesquisa de distância, sem a necessidade de uma estrutura prévia."
-        imageSrc={images}
+        title="Conteúdo visual de forma direta"
+        description="Em vez de precisar lidar diretamente com código ou linguagens de marcação, o UpWrite permite que os usuários formem textos, criem listas e realizem outras tarefas de formatação de maneira intuitiva, sem exigir um conhecimento profundo em HTML, CSS ou outras linguagens de marcação."
+        imageSrc={imageSrc}
         imageWidth={1102}
         imageHeight={810}
         imagePosition="right"
@@ -23,4 +32,4 @@ const Organizaçao = () => {
   );
 };
 
-export default Organizaçao;
+export default Organizacao;
